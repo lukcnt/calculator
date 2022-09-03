e1bc867200a45e170e5cd8f01e64fc951e1e7e5d
Lucas Cantoni <lukcnt@outlook.com>
2022-09-03 00:08:53 -0300

    Create the subtraction function that find the difference of two numbers

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def addition(first_number, second_number):
     sum = first_number + second_number
     return sum
+
+def subtraction(first_number, second_number):
+    difference = first_number - second_number
+    return difference
